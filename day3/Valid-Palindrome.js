222a4315e9213e2d62a8a098542623cdbc3bcadd
Youssefmohamed1237 <118205824+Youssefmohamed1237@users.noreply.github.com>
2026-02-22 17:20:39 +0200

    [125] [Time Beats: 100.00%] [Memory Beats: 84.78%] - LeetPush

--- day3/Valid-Palindrome.js
+++ day3/Valid-Palindrome.js
@@ -1,11 +1,11 @@
 1function isPalindrome(str) {
-2  const s = str
+2  const sarr = str
 3    .toLowerCase()
 4    .replace(/[^a-z0-9]/g, "")
 5    .split("");
-6  let j = s.length - 1;
-7  for (i = 0; i < s.length; i++) {
-8    if (!(s[i] == s[j])) return false;
+6  let j = sarr.length - 1;
+7  for (i = 0; i < sarr.length; i++) {
+8    if (!(sarr[i] == sarr[j])) return false;
 9    j--;
 10  }
 11  return true;
